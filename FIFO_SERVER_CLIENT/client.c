333000f727fc769f44ee3425c1722b5f14b3a121
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-11-08 12:43:52 +0530

    Create client.c

---
+++ FIFO_SERVER_CLIENT/client.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+#define FIFO1 "fifo1"
+#define FIFO2 "fifo2"
+#define PERMS 0666
+char fname[256];
+int main()
+{
+	int readfd,writefd,fd;
+	ssize_t n;
+	char buff[512]; 
+	
+	printf("Trying to connect to server..\n");
+	writefd=open(FIFO1,O_WRONLY,0);
+	readfd=open(FIFO2,O_RDONLY,0);
+	
+	printf("Connected..\n");
+	printf("Enter the filename to request from server:");
+	scanf("%s",fname);
+	write(writefd,fname,strlen(fname));
+	
+	printf("Waiting for server to reply..\n");
+	while((n=read(readfd,buff,512))>0)
+		write(1,buff,n);
+	close(readfd);
+	close(writefd);
+	return 0;
+}
+/*
+OUTPUT:
+Trying to connect to server..
+Connected..
+Enter the filename to request from server:texxt.txt
+Waiting for server to reply..
+Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah 
+*/
